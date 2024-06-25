70fa81f7fced16e2324297d2940ee3401b2bacc3
papel <xDNdnQoyTcrrMdWCVZft4YLqkaRWnRb@protonmail.com>
2024-06-25 14:45:41 +0200

    Switch back to MG -> solution looking good

--- cpp/demo/geometric-multigrid/main.cpp
+++ cpp/demo/geometric-multigrid/main.cpp
@@ -6,6 +6,7 @@
 #include <mpi.h>
 #include <numbers>
 #include <petscksp.h>
+#include <petsclog.h>
 #include <petscmat.h>
 #include <petscpc.h>
 #include <petscpctypes.h>
@@ -35,10 +36,11 @@ using U = typename dolfinx::scalar_value_type_t<T>;
 
 int main(int argc, char** argv)
 {
-    // int n_coarse = 8;
+    int n_coarse = 8;
     int n_fine = 16;
     
     PetscInitialize(&argc, &argv, nullptr, nullptr);
+    // PetscLogDefaultBegin();
 
     auto mesh = std::make_shared<mesh::Mesh<U>>(
         dolfinx::mesh::create_interval<U>(MPI_COMM_SELF, n_fine, {0.0, 1.0}));
@@ -103,51 +105,51 @@ int main(int argc, char** argv)
     PC pc;
     KSPGetPC(ksp, &pc);
     KSPSetFromOptions(ksp);
-    PCSetType(pc, "lu");
+    PCSetType(pc, "mg");
 
-    // PCMGSetLevels(pc, 2, NULL);
-    // PCMGSetType(pc, PC_MG_MULTIPLICATIVE);
-    // PCMGSetCycleType(pc, PC_MG_CYCLE_V);
-    // PCMGSetGalerkin(pc, PC_MG_GALERKIN_BOTH);
-    // PCMGSetNumberSmooth(pc, 2);
-    // PCSetFromOptions(pc);
+    PCMGSetLevels(pc, 2, NULL);
+    PCMGSetType(pc, PC_MG_MULTIPLICATIVE);
+    PCMGSetCycleType(pc, PC_MG_CYCLE_V);
+    PCMGSetGalerkin(pc, PC_MG_GALERKIN_BOTH);
+    PCMGSetNumberSmooth(pc, 2);
+    PCSetFromOptions(pc);
 
-    // Mat interpolation;
-    // {
-    //     int64_t nz = 2*n_fine + (n_fine+1);
-    //     std::vector<PetscInt> i; // row indices
-    //     i.reserve(nz);
-    //     std::vector<PetscInt> j; // col indices
-    //     j.reserve(nz);
-    //     std::vector<PetscScalar> a;
-    //     a.reserve(nz);
-    //     for (int64_t idx = 0; idx < n_fine+1; idx ++)
-    //     {
-    //         if (idx % 2 == 0)
-    //         {
-    //             i.emplace_back(idx);
-    //             j.emplace_back(PetscInt(idx/2.));
-    //             a.emplace_back(1);
-    //         } else {
-    //             i.emplace_back(idx);
-    //             j.emplace_back(floor(idx/2.));
-    //             a.emplace_back(.5);            
+    Mat interpolation;
+    {
+        int64_t nz = 2*n_fine + (n_fine+1);
+        std::vector<PetscInt> i; // row indices
+        i.reserve(nz);
+        std::vector<PetscInt> j; // col indices
+        j.reserve(nz);
+        std::vector<PetscScalar> a;
+        a.reserve(nz);
+        for (int64_t idx = 0; idx < n_fine+1; idx ++)
+        {
+            if (idx % 2 == 0)
+            {
+                i.emplace_back(idx);
+                j.emplace_back(PetscInt(idx/2.));
+                a.emplace_back(1);
+            } else {
+                i.emplace_back(idx);
+                j.emplace_back(floor(idx/2.));
+                a.emplace_back(.5);            
 
-    //             i.emplace_back(idx);
-    //             j.emplace_back(ceil(idx/2.));
-    //             a.emplace_back(.5);
-    //         }
-    //     }
-    //     MatCreateSeqAIJFromTriple(MPI_COMM_SELF, n_fine+1, n_coarse+1, i.data(), j.data(), a.data(), &interpolation, a.size(), PETSC_FALSE);
-    //     MatView(interpolation, PETSC_VIEWER_STDOUT_SELF);
-    // }
+                i.emplace_back(idx);
+                j.emplace_back(ceil(idx/2.));
+                a.emplace_back(.5);
+            }
+        }
+        MatCreateSeqAIJFromTriple(MPI_COMM_SELF, n_fine+1, n_coarse+1, i.data(), j.data(), a.data(), &interpolation, a.size(), PETSC_FALSE);
+        MatView(interpolation, PETSC_VIEWER_STDOUT_SELF);
+    }
 
-    // Mat restriction;
-    // MatTranspose(interpolation, MAT_INITIAL_MATRIX, &restriction);
-    // MatView(restriction, PETSC_VIEWER_STDOUT_SELF);
+    Mat restriction;
+    MatTranspose(interpolation, MAT_INITIAL_MATRIX, &restriction);
+    MatView(restriction, PETSC_VIEWER_STDOUT_SELF);
 
-    // PCMGSetInterpolation(pc, 1, interpolation);
-    // PCMGSetRestriction(pc, 1, restriction);
+    PCMGSetInterpolation(pc, 1, interpolation);
+    PCMGSetRestriction(pc, 1, restriction);
 
     MatView(A.mat(), PETSC_VIEWER_STDOUT_SELF);
 
@@ -159,18 +161,18 @@ int main(int argc, char** argv)
     la::petsc::Vector _u(la::petsc::create_vector_wrap(*u->x()), false);
     la::petsc::Vector _b(la::petsc::create_vector_wrap(b), false);
 
-    VecView(_b.vec(), PETSC_VIEWER_STDOUT_SELF);
+    // VecView(_b.vec(), PETSC_VIEWER_STDOUT_SELF);
 
     KSPSolve(ksp, _b.vec(), _u.vec());
     // Update ghost values before output
     u->x()->scatter_fwd();
-    VecView(_u.vec(), PETSC_VIEWER_STDOUT_SELF);
+    // VecView(_u.vec(), PETSC_VIEWER_STDOUT_SELF);
 
     {
         io::VTKFile file(MPI_COMM_SELF, "u.pvd", "w");
         file.write<T>({*u}, 0.0);
     }
-    PCView(pc, PETSC_VIEWER_STDERR_SELF);
-
+    // PCView(pc, PETSC_VIEWER_STDERR_SELF);
+    
     KSPDestroy(&ksp);
 }
